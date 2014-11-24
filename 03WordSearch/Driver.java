ba6da3bc4c9d82f2cd797ce9bfc88621dbde35c6
Wayez <c.wayez3793@gmail.com>
2014-11-23 23:25:45 -0500

    Made small modifications

--- 03WordSearch/Driver.java
+++ 03WordSearch/Driver.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Driver{
     public static WordGrid start(){
@@ -7,26 +8,34 @@ public class Driver{
 	int row, col;
 	System.out.println("***Welcome to Wayez's Wordsearch***");
 	for (int i=0; i>=0; i++){
+	    System.out.println();
 	    System.out.println("Set the Dimensions of your Wordsearch");
 	    System.out.println("Set Height(5 min, 20 max)");
-	    Scanner size=new Scanner(System.in);
-	    row=size.nextInt();
-	    System.out.println("Set Width(5 min, 50 max)");
-	    col=size.nextInt();
-	    if (col>=5 && col<=50 && row>=5 && row<=20){
-		A=new WordGrid(row, col);
-		break;
-	    } else {
-		System.out.println("You're Dumb");
+	    try{
+		Scanner size=new Scanner(System.in);
+		row=size.nextInt();
+		System.out.println("Set Width(5 min, 50 max)");
+		col=size.nextInt();
+		if (col>=5 && col<=50 && row>=5 && row<=20){
+		    A=new WordGrid(row, col);
+		    break;
+		} else {
+		    System.out.println("You're Dumb");
+		}
+	    } catch (InputMismatchException e){
+		System.out.println("You need to go back to Kindergarden and learn what a number is.");
+	    
 	    }
 	}
 	return A;	
+	
     }
 	
 	    
     public static void main(String[]args){
 	WordGrid A=start();
 	A.setWordSearch();
+	//A.play();
 	System.out.println(A.WordBox());
 	System.out.println();
 	System.out.println();
