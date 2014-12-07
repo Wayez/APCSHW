10d8c569bf637462b231da7b4d57772bd0221c6c
Wayez <c.wayez3793@gmail.com>
2014-12-07 16:17:26 -0500

    Added Arguments

--- 04OrderedSuperArray/Driver.java
+++ 04OrderedSuperArray/Driver.java
@@ -1,36 +1,14 @@
 public class Driver{
-    public static void main(String[]superArraysAreAwesome){
-	SuperArray B=new SuperArray();
-	B.add(0, "k");
-	OrderedSuperArray Wayez=new OrderedSuperArray(10);
-	System.out.println(B.toString());
-	//System.out.println(Wayez.size());
-	Wayez.add("Apple");
-	System.out.println(Wayez.toString());
-	//System.out.println(Wayez.size());
-	Wayez.add("Banana");
-	//System.out.println(Wayez.toString());
-	//System.out.println(Wayez.size());
-	Wayez.add("Orange");
-	//System.out.println(Wayez.toString());
-	//System.out.println(Wayez.size());
-	Wayez.add("Pear");
-	Wayez.add("Grape");
-	Wayez.add("Cherry");
-	Wayez.add("Pineapple");
-	Wayez.add("Watermellon");
-	Wayez.add("Mango");
-	System.out.println(Wayez.toString());
-	//SuperArray numbers=new SuperArray("numbers.txt");
-	SuperArray numbers=new SuperArray();
-	for (int x=0; x<100; x++){
-	    numbers.add(Integer.toString(x));
+    public static SuperArray start(String[] args){
+	SuperArray ans=new SuperArray();
+	if (args.length==1){
+	    return ans;
+	} else {
+	    System.out.println("Choose First Args:\n1: insertionSort\n2: selectionSort\n3: Arrays.sort()\n4: OrderedArrayList");
+	    return null;
 	}
-	//numbers.InsertionSort();
-	numbers.SelectionSort();
-	System.out.println(numbers.toString());
-	//System.out.println("2".compareTo("10"));
-	//System.out.println("2".compareTo("20"));
-	//System.out.println("2".compareTo("29"));
     }
+    public static void main(String[]args){
+	SuperArray Wayez =start(args);
+    }	
 }
